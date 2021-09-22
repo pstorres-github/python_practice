a73843f0ea8c4d087e9cf2ade093c376fef4f56f
PTorres <pstorres@gmail.com>
2021-09-22 14:22:22 -0600

    Moved class into another file, import to main

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-# classes
-
-# capitalize first letter of every word (Pascal case)
-class Mammal:
-    # initialize the object (constructor)
-    def __init__(self, name):
-        self.name = name
-
-    def walk(self):
-        print("walk")
-
-    def printName(self):
-        return(self.name)
-
-
-# need two line breaks between classes!!
-# inheritance.  Both dog and cat class are inheriting from Mammal Class
-class Dog(Mammal):
-    def bark(self):
-        print("bark")
-
-# if no added functions, then pass
-class Cat(Mammal):
-    def meow(self):
-        print("meow")
-
-
-# if no added functions, then pass
-class Cow(Mammal):
-    pass
-
+from Mammal import Dog, Cat, Cow
 
 # create object
 dog = Dog("Cassie")
