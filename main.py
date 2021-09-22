fdfc581dd1b9514315d6733aa43814694be319b8
PTorres <pstorres@gmail.com>
2021-09-22 15:23:11 -0600

    Excel file processing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from Mammal import Dog, Cat, Cow
+# import each class from package
+from animals.Mammal import Dog, Cat, Cow
+from Dice import Dice
+from pathlib import Path
 
-# create object
+# --------------------------------
 dog = Dog("Cassie")
 print("Hello", dog.printName())
 dog.walk()
@@ -11,3 +14,23 @@ cat.meow()
 
 cow = Cow("Moo cow")
 print("Hello", cow.printName())
+# -----------------------------------
+
+print("")
+print("Dice roll activity")
+dice = Dice()
+dice.roll()
+
+# ------------------------------------
+
+print("")
+print("search for all python files")
+
+path = Path()
+for file in path.glob('*.py'): # search for all files in the current path with extension *.py
+    print(file)
+
+# ---------------------------------
+# installing packages
+# terminal command: pip install "package", for example pip install openpyxl
+
